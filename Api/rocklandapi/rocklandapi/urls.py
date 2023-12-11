c093c4c0ccdcfc071f2f54a0abf519e3df021210
shirinnn <shirinoh88@gmail.com>
2023-12-11 13:09:31 +0800

    able to add an account

--- Api/rocklandapi/rocklandapi/urls.py
+++ Api/rocklandapi/rocklandapi/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', views.account_list)
+    path('api/accounts/', views.account_list)
 ]
--- Api/rocklandapi/rocklandapi/views.py
+++ Api/rocklandapi/rocklandapi/views.py
@@ -1,18 +1,29 @@
 from django.http import JsonResponse
 from .models import Accounts
 from .serializers import AccountSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # create endpoints here
 # endpoints = certain url we can access the data from
 
+@api_view(['GET', 'POST'])
 def account_list(request):
 
-    # get all account
-    # serialize them
-    # return json
-
-    accounts = Accounts.objects.all()
-
-    accSerializer = AccountSerializer(accounts, many=True)
-
-    return JsonResponse({'accounts': accSerializer.data}, safe=False)
+    if request.method == 'GET':
+        # get all account
+        accounts = Accounts.objects.all()
+        # serialize them
+        accSerializer = AccountSerializer(accounts, many=True)
+        # return json
+        return JsonResponse({'accounts': accSerializer.data})
+    
+    if request.method == 'POST':
+        # take the data send over
+        # deserialize it
+        # create a object out of it
+        accSerializer = AccountSerializer(data = request.data)
+        if accSerializer.is_valid():
+            accSerializer.save()
+            return Response(accSerializer.data, status=status.HTTP_201_CREATED)
