9ae5abfba021034f111354c049d952b162200406
shirinnn <shirinoh88@gmail.com>
2024-02-21 00:41:54 +0800

    text decoration changes to forum thread link

--- react/rockland-react/src/components/ForumThreadsComponent.jsx
+++ react/rockland-react/src/components/ForumThreadsComponent.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function ForumThreadsComponent(props) {
   return (
     <div>
-      <Link to={`/forumComments/${props.thread_id}`}>
+      <Link to={`/forumComments/${props.thread_id}`} style={{textDecoration: "none", color: "black"}}>
         <Card key={props.thread_id}>
           <Card.Body>
               <Card.Title>{props.title}</Card.Title>
